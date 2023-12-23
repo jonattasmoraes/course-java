be18b1db91c85ec6e8c4d03d771db19f3392d55e
Jônattas Moraes <jonatas.moraes.09@hotmail.com>
2023-12-23 12:30:37 -0300

    feat: add TarefaControleFluxo

--- src/TarefaControleFluxo.java
+++ src/TarefaControleFluxo.java
@@ -1,6 +1,32 @@
+
 /**
  * @author Jonattas Moraes
  */
-public class TarefaControleFluxo {
+import java.util.Scanner;
 
+public class TarefaControleFluxo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double[] notas = new double[4];
+
+        for (int i = 0; i < 4; i++) {
+            System.out.print("Digite a nota " + (i + 1) + ": ");
+            notas[i] = scanner.nextDouble();
+        }
+
+        double media = (notas[0] + notas[1] + notas[2] + notas[3]) / 4;
+
+        System.out.println("Média: " + media);
+
+        if (media >= 7) {
+            System.out.println("Aluno Aprovado!");
+        } else if (media >= 5 && media < 7) {
+            System.out.println("Aluno em Recuperação.");
+        } else {
+            System.out.println("Aluno Reprovado.");
+        }
+
+        scanner.close();
+    }
 }
