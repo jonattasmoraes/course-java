8dbddfb54bddc32dcd6174cb30b0546b06d8a0f2
Jônattas Moraes <jonatas.moraes.09@hotmail.com>
2023-12-24 08:34:06 -0300

    feat: add ColecoesDois and NomesOrdenados

--- src/ColecoesDois.java
+++ src/ColecoesDois.java
@@ -1,3 +1,44 @@
-public class ColecoesDois {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
+public class ColecoesDois {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, String> nomesPorGenero = new HashMap<>();
+
+        while (true) {
+            System.out.print("Digite o nome e sexo separados por '-' (ou 'sair' para encerrar): ");
+            String entrada = scanner.nextLine();
+
+            if (entrada.equalsIgnoreCase("sair")) {
+                break;
+            }
+
+            String[] partes = entrada.split("-");
+            if (partes.length == 2) {
+                String nome = partes[0].trim();
+                String sexo = partes[1].trim().toUpperCase();
+
+                nomesPorGenero.put(nome, sexo);
+            } else {
+                System.out.println("Entrada inválida. Digite o nome e sexo separados por '-'.");
+            }
+        }
+
+        exibirNomesPorGenero(nomesPorGenero, "M", "Masculinos");
+        exibirNomesPorGenero(nomesPorGenero, "F", "Femininos");
+
+        scanner.close();
+    }
+
+    private static void exibirNomesPorGenero(Map<String, String> nomesPorGenero, String genero, String mensagem) {
+        System.out.println("\n" + mensagem + ":");
+        nomesPorGenero.forEach((nome, sexo) -> {
+            if (sexo.equalsIgnoreCase(genero)) {
+                System.out.println(nome);
+            }
+        });
+    }
 }
--- src/NomesOrdenados.java
+++ src/NomesOrdenados.java
@@ -1,3 +1,27 @@
-public class NomesOrdenados {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class NomesOrdenados {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite os nomes separados por vírgula: ");
+        String nomesInput = scanner.nextLine();
+
+        String[] nomes = nomesInput.split(",");
+
+        for (int i = 0; i < nomes.length; i++) {
+            nomes[i] = nomes[i].trim();
+        }
+
+        Arrays.sort(nomes);
+
+        System.out.println("\nNomes ordenados:");
+
+        for (String nome : nomes) {
+            System.out.println(nome);
+        }
+
+        scanner.close();
+    }
 }
